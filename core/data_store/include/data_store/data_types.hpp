9fdd0909c2005c8c8bf207f5a112be5820cee58b
Karol Frankowski <karol.j.frankowki@gmail.com>
2021-07-27 13:42:31 +0200

    changes serviced to match drawio agreement

--- core/data_store/include/data_store/data_types.hpp
+++ core/data_store/include/data_store/data_types.hpp
@@ -1,23 +1,6 @@
 #ifndef _DATA_TYPES_HPP
 #define _DATA_TYPES_HPP
 #include "data_element.hpp"
-//rgb
-#include "custom_interfaces/srv/data_store_rgb_data_insert.hpp"
-#include "custom_interfaces/srv/data_store_rgb_data_delete.hpp"
-#include "custom_interfaces/srv/data_store_rgb_data_select.hpp"
-#include "custom_interfaces/srv/data_store_rgb_data_get_list.hpp"
-
-//item_cam1
-#include "custom_interfaces/srv/data_store_item_cam1_insert.hpp"
-#include "custom_interfaces/srv/data_store_item_cam1_delete.hpp"
-#include "custom_interfaces/srv/data_store_item_cam1_select.hpp"
-#include "custom_interfaces/srv/data_store_item_cam1_get_list.hpp"
-
-//item_cam2
-#include "custom_interfaces/srv/data_store_item_cam2_insert.hpp"
-#include "custom_interfaces/srv/data_store_item_cam2_delete.hpp"
-#include "custom_interfaces/srv/data_store_item_cam2_select.hpp"
-#include "custom_interfaces/srv/data_store_item_cam2_get_list.hpp"
 
 //tracker
 #include "custom_interfaces/srv/data_store_tracker_insert.hpp"
@@ -25,30 +8,28 @@
 #include "custom_interfaces/srv/data_store_tracker_select.hpp"
 #include "custom_interfaces/srv/data_store_tracker_get_list.hpp"
 
+//detectron
+#include "custom_interfaces/srv/data_store_detectron_insert.hpp"
+#include "custom_interfaces/srv/data_store_detectron_delete.hpp"
+#include "custom_interfaces/srv/data_store_detectron_select.hpp"
+#include "custom_interfaces/srv/data_store_detectron_get_list.hpp"
+
+//items
+#include "custom_interfaces/srv/data_store_items_insert.hpp"
+#include "custom_interfaces/srv/data_store_items_delete.hpp"
+#include "custom_interfaces/srv/data_store_items_select.hpp"
+#include "custom_interfaces/srv/data_store_items_get_list.hpp"
+
+//cameras_data
+#include "custom_interfaces/srv/data_store_cameras_data_insert.hpp"
+#include "custom_interfaces/srv/data_store_cameras_data_delete.hpp"
+#include "custom_interfaces/srv/data_store_cameras_data_select.hpp"
+#include "custom_interfaces/srv/data_store_cameras_data_get_list.hpp"
+
 // add other data elements here
 
 namespace data_store
 {
-    //rgb
-    using RgbDataInsert = custom_interfaces::srv::DataStoreRgbDataInsert;
-    using RgbDataDelete = custom_interfaces::srv::DataStoreRgbDataDelete;
-    using RgbDataSelect = custom_interfaces::srv::DataStoreRgbDataSelect;
-    using RgbDataGetList = custom_interfaces::srv::DataStoreRgbDataGetList;
-    using RgbData = DataElement<RgbDataSelect, RgbDataInsert, RgbDataGetList, RgbDataDelete>;
-
-    //item_cam1
-    using ItemCam1Insert = custom_interfaces::srv::DataStoreItemCam1Insert;
-    using ItemCam1Delete = custom_interfaces::srv::DataStoreItemCam1Delete;
-    using ItemCam1Select = custom_interfaces::srv::DataStoreItemCam1Select;
-    using ItemCam1GetList = custom_interfaces::srv::DataStoreItemCam1GetList;
-    using ItemCam1 = DataElement<ItemCam1Select, ItemCam1Insert, ItemCam1GetList, ItemCam1Delete>;
-
-    //item_cam2
-    using ItemCam2Insert = custom_interfaces::srv::DataStoreItemCam2Insert;
-    using ItemCam2Delete = custom_interfaces::srv::DataStoreItemCam2Delete;
-    using ItemCam2Select = custom_interfaces::srv::DataStoreItemCam2Select;
-    using ItemCam2GetList = custom_interfaces::srv::DataStoreItemCam2GetList;
-    using ItemCam2 = DataElement<ItemCam2Select, ItemCam2Insert, ItemCam2GetList, ItemCam2Delete>;
 
     //tracker
     using TrackerInsert = custom_interfaces::srv::DataStoreTrackerInsert;
@@ -56,6 +37,27 @@ namespace data_store
     using TrackerSelect = custom_interfaces::srv::DataStoreTrackerSelect;
     using TrackerGetList = custom_interfaces::srv::DataStoreTrackerGetList;
     using Tracker = DataElement<TrackerSelect, TrackerInsert, TrackerGetList, TrackerDelete>;
+
+    //cameras_data
+    using CamerasDataInsert = custom_interfaces::srv::DataStoreCamerasDataInsert;
+    using CamerasDataDelete = custom_interfaces::srv::DataStoreCamerasDataDelete;
+    using CamerasDataSelect = custom_interfaces::srv::DataStoreCamerasDataSelect;
+    using CamerasDataGetList = custom_interfaces::srv::DataStoreCamerasDataGetList;
+    using CamerasData = DataElement<CamerasDataSelect, CamerasDataInsert, CamerasDataGetList, CamerasDataDelete>;
+
+    //detectron
+    using DetectronInsert = custom_interfaces::srv::DataStoreDetectronInsert;
+    using DetectronDelete = custom_interfaces::srv::DataStoreDetectronDelete;
+    using DetectronSelect = custom_interfaces::srv::DataStoreDetectronSelect;
+    using DetectronGetList = custom_interfaces::srv::DataStoreDetectronGetList;
+    using Detectron = DataElement<DetectronSelect, DetectronInsert, DetectronGetList, DetectronDelete>;
+
+    //items
+    using ItemsInsert = custom_interfaces::srv::DataStoreItemsInsert;
+    using ItemsDelete = custom_interfaces::srv::DataStoreItemsDelete;
+    using ItemsSelect = custom_interfaces::srv::DataStoreItemsSelect;
+    using ItemsGetList = custom_interfaces::srv::DataStoreItemsGetList;
+    using Items = DataElement<ItemsSelect, ItemsInsert, ItemsGetList, ItemsDelete>;
 
     // add other data elements here
 
