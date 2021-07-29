d553de38a57640c2ec9a87ae8117cfd95b32d607
HasanFarag <hasanfarag@aol.com>
2021-07-29 09:35:48 +0200

    add examples back

---
+++ core/data_store/examples/scripts/cam2/get_lists.py
@@ -0,0 +1,46 @@
+from custom_interfaces.srv import DataStoreItemCam2GetList
+import rclpy
+from rclpy.node import Node
+
+
+class MinimalClientAsync(Node):
+
+    def __init__(self):
+        super().__init__('rgb_delete_client_async')
+        self.cli = self.create_client(DataStoreItemCam2GetList, 'item_cam_2_get_list')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = DataStoreItemCam2GetList.Request()
+
+    def send_request(self):
+        self.future = self.cli.call_async(self.req)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_client = MinimalClientAsync()
+    minimal_client.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(minimal_client)
+        if minimal_client.future.done():
+            try:
+                response = minimal_client.future.result()
+            except Exception as e:
+                minimal_client.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                if len(response.time_stamps)!=0:
+                    for time_stamp in response.time_stamps:
+                        minimal_client.get_logger().info(str(time_stamp))
+                else:
+                    minimal_client.get_logger().info(str(response.time_stamps))
+            break
+
+    minimal_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
