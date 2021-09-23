60e0543d5150505230baac446aba65b661187481
Avena Robotics <lukasz.lecki@pomagier.info>
2021-09-23 08:19:04 +0000

    Update baremetal.sh

--- install/baremetal.sh
+++ install/baremetal.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-sudo apt-get install openssh-server -y
-sudo apt-get install mc -y
-sudo apt-get install nmap -y
-sudo apt-get install git -y
+#few tools
+apt-get install openssh-server -y
+apt-get install mc -y
+apt-get install nmap -y
+apt-get install git -y
 
 # docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # nvidia docker
 curl https://get.docker.com | sh
-sudo systemctl start docker && sudo systemctl enable docker
+systemctl start docker && sudo systemctl enable docker
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
-sudo apt-get install -y nvidia-docker2
-sudo systemctl restart docker
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+apt-get update
+apt-get install -y nvidia-docker2
+systemctl restart docker
 
+#add local repository server (avena LAN only)
 echo '{
     "insecure-registries":["10.3.14.14:5000"],
     "runtimes": {
@@ -31,7 +33,11 @@ echo '{
 
 sudo systemctl restart docker
 
+#clean up all dockers - nevermind when errors
 docker stop -t 5 ${docker ps -aq}
 docker rm -f ${docker ps -aq}
 docker rmi ${docker images -aq}
+
+#install newest nvidia drivers
+apt install nvidia-utils-470
 
