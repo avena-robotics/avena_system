b3c11b863ecbf79c16af62b80cbcd714bb76ac94
Arek <arkadiusz.ziehm@pomagier.info>
2022-02-25 08:20:56 +0100

    debug

--- drivers/arm_controller/src/hw_interface.cpp
+++ drivers/arm_controller/src/hw_interface.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc,char **argv){
     rclcpp::init(argc,argv);
-    ArmInterface hw_interface("0AA");
+    ArmInterface hw_interface("000");
     // rclcpp::spin(std::make_shared<ArmInterface>("0AA"));
     return 0;
 }
